8f8444d0561d0488004155c6ae7d0f5a4bb64a68
cherrykot <kot.aleksei43@gmail.com>
2019-09-15 20:26:24 +0300

    fix

--- script_lx.js
+++ script_lx.js
@@ -11,7 +11,7 @@ function createMonth(year1,month1,day1) {
 	let textMonth = date.toLocaleString('ru',{month: 'long'})[0].toUpperCase() + date.toLocaleString('ru',{month: 'long'}).slice(1);
 	let lastDay = new Date(year,month+1,0).getDate(); // для получения последнего числа в месяце
 	let day_Of_Week_First_Day_In_Month = new Date(year,month,1).getDay(); // для получения дня недели первого дня месяца
-	
+
 	
 
 	let itMonth = document.createElement('h1'),
@@ -69,30 +69,13 @@ for(let i = 0; i < 7; i++){
 			li.innerHTML = '&nbsp';
 		else 
 			li.innerHTML = start++;
-		if(dayGlobal == today && monthGlobal == month && today == i &&  yearGlobal == year)
+		if(dayGlobal == today && monthGlobal == month && today == (i - emptyLi + 1) &&  yearGlobal == year){
 			li.className = 'today';
+		}
 		ulDays.append(li);
 	}
+	
 }
 
 createMonth(yearGlobal,monthGlobal,dayGlobal);
 
-// function weatherCall(){
-//     let cityId = 703447;
-//     let units = 'metric';
-//     let apiKey = '286e0e247b1e430ff91e8bbb945cf78d';
-
-//     let requestString = `https://api.openweathermap.org/data/2.5/weather?id=${cityId}&appid=${apiKey}&units=${units}`;
-//     request = new XMLHttpRequest();
-//     request.open("get", requestString, true);
-//     request.send();
-//     let weatherObject;
-//     request.onload = function (){
-//         weatherObject = JSON.parse(this.responseText);
-//         document.querySelector(".weather__span").innerHTML = Math.round(weatherObject.main.temp) + 'º';
-    
-//     }
-
-// }
-
-// setInterval(weatherCall(), 1000*60*15);
